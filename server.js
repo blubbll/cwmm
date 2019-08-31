1e66f2c1e79b294fab82c10cd3fb6f30647538c6
Glitch (cwmm) <none>
2019-08-31 14:02:53 +0000

    🙍🍏 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,229 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+const fetch = require("node-fetch")
+const request = require("request")
+const fs = require("fs")
+const LocalStorage = require("node-localstorage").LocalStorage;
+
+//ls
+if (typeof localStorage === "undefined" || localStorage === null) {
+    localStorage = new LocalStorage('./scratch');
+}
+
+
+const getDate = () => {
+    return new Date().toISOString().slice(0, 10).replace(/-/g, '')
+};
+
+const quota = {
+    set: (c) => {
+        localStorage.setItem(`dls_${getDate()}`, c);
+    },
+    get: () => {
+        return localStorage.getItem(`dls_${getDate()}`)
+    },
+    add: () => {
+        let c = localStorage.getItem(`dls_${getDate()}`);
+        localStorage.setItem(`dls_${getDate()}`, (+c) + 1);
+    },
+    substract: () => {
+        let c = localStorage.getItem(`dls_${getDate()}`);
+        localStorage.setItem(`dls_${getDate()}`, (+c) - 1);
+    }
+}
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
+
+const get = async (file) => new Promise((resolve, reject) => {
+    request({
+        url: 'https://api.beta.aiva.ai/composition/play',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': process.env.TOKEN
+        },
+        json: {
+            compositionID: file
+        }
+    }, (error, response, body) => {
+
+    }).on('response', async response => {
+        response.pipe(fs.createWriteStream(`tmp/${file}.mp3`));
+
+        await upload({
+            host: process.env.FTP_HOST,
+            port: 21,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASS,
+        }, getDate(), `${file}.mp3`);
+
+        resolve();
+
+    })
+    //}).pipe(fs.createWriteStream(`${getDate()}/${file}.mp3`))
+});
+
+//dl('5d5bb9d09fa808020acde4d2');
+
+
+//************ FTP custom (requires 'ftp')
+const upload = async (credentials, pathToLocalFile, pathToRemoteFile) => new Promise((resolve, reject) => {
+    var Client = require('ftp');
+    var options = {
+        host: credentials.host,
+        port: credentials.port,
+        user: credentials.user,
+        password: credentials.password
+    };
+    var c = new Client();
+    //on client ready, upload the file.
+    c.on('ready', () => {
+        console.log(`[start]uploading ${pathToLocalFile} => ${pathToRemoteFile}`)
+        c.put(pathToLocalFile, pathToRemoteFile, function(err) {
+            c.end(); //end client
+            fs.unlink(pathToLocalFile, (error) => {
+                /* handle error */
+            });
+            console.log(`[done]uploaded ${pathToLocalFile} => ${pathToRemoteFile}`)
+            if (err) reject(err); //reject promise
+            resolve(); //fullfill promise
+        });
+    });
+    //general error
+    c.on('error', (err) => {
+        return reject(err);
+    });
+    c.connect(options);
+});
+
+const deleteSong = (id => new Promise(async (resolve, reject) => {
+    request({
+        url: 'https://api.beta.aiva.ai/composition/delete',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': process.env.TOKEN
+        },
+        json: {
+            compositionID: id
+        }
+    }, (error, response, song) => {
+
+        if (error === null) {
+            console.log(`${song.name} wurde gelöscht`);
+            resolve();
+        } else {
+            console.error(error);
+            return reject(error);
+        }
+        //create();
+    });
+}));
+const create = () => new Promise(async (resolve, reject) => {
+    if (quota.get() < 200)
+        request({
+            url: 'https://api.beta.aiva.ai/composition/original/createFromPreset',
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': process.env.TOKEN
+            },
+            json: {
+                duration: "auto",
+                ensemble: "auto",
+                folderID: "",
+                includeDrums: true,
+                includeTempoMap: true,
+                key: "auto",
+                numberOfCompositions: "1",
+                pacing: "auto",
+                preset: "fantasy",
+                timeSignature: "auto",
+                token: process.env.TOKEN
+            }
+        }, (error, response, body) => {
+            if (error === null) {
+                const song = body.compositions[0];
+                console.log(`#${song._id} (${song.name}) wird erstellt`);
+                quota.add();
+                resolve();
+            }
+          else {
+            console.error(error);
+            return reject(error);
+        }
+        });
+    else console.log("Daily quota reached.")
+});
+
+const pumpSongs = () => {
+    request({
+        url: 'https://api.beta.aiva.ai/folder/getContent',
+        "referrer": "https://beta.aiva.ai/",
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': process.env.TOKEN
+        },
+        json: {
+            folderID: "",
+            token: process.env.TOKEN
+        }
+    }), async (error, response, json) => {
+
+        console.log("pumping songs")
+
+        var i = 0;
+
+        if(json.compositions.length>1)
+        //Songs löschen
+        for (var key in json.compositions) {
+            const song = json.compositions[key];
+            console.log(`deleting ${song.name}`)
+
+            if (song.isFinished) //done
+
+                await upload({
+                  host: process.env.FTP_HOST,
+                  port: 21,
+                  user: process.env.FTP_USER,
+                  password: process.env.FTP_PASS,
+              }, getDate(), `${song.name}.mp3`);
+          
+                await deleteSong(song._id);
+          
+          i++;
+          
+          if(i === json.compositions){
+            console.log("all old songs have been uploaded.");
+            
+            create();
+            
+          }
+          
+        } else create(); //create new if not existing
+    }
+};
+
+//create();
+//clearAll()
+setInterval(() => {
+    pumpSongs();
+}, 9999)
+
+//
